cb28d4527643a9529aca79f3bf1a773d79ab658e
Eugene Kapelko <softwarearchitect19@gmail.com>
2024-07-03 22:09:22 +0300

    Create server with Pat. Create template with Jet.

---
+++ cmd/web/routes.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/bmizerany/pat"
+	"go-websocket-chat/internal/handlers"
+	"net/http"
+)
+
+func routes() http.Handler {
+	mux := pat.New()
+
+	mux.Get("/", http.HandlerFunc(handlers.Home))
+
+	return mux
+}
